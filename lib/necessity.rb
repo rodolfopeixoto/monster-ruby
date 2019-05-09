72dbadfb537e43c53cb728a939e06194e9998cbb
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2019-05-09 18:50:39 -0300

    Refactory method level_max_two_item?

--- lib/necessity.rb
+++ lib/necessity.rb
@@ -25,21 +25,26 @@ class Necessity
   end
 
   def time_hygiene
-    return @hygiene *= 2 if (@food == 4 || @fun == 4) && @hygiene < 3 && @hygiene > 0
+    return @hygiene *= 2 if level_max_two_item?(@food, @fun) && @hygiene < 3 && @hygiene > 0
     return @hygiene += 1 if (@food != 4 || @fun != 4) && @hygiene < 4
-    return @hygiene += 1 if (@food == 4 || @fun == 4) && (@hygiene == 3 || @hygiene.zero?)
+    return @hygiene += 1 if level_max_two_item?(@food, @fun) && (@hygiene == 3 || @hygiene.zero?)
   end
 
   def time_fun
-    return @fun *= 2 if (@hygiene == 4 || @food == 4) && @fun < 3 && @fun > 0
+    return @fun *= 2 if level_max_two_item?(@hygiene, @food) && @fun < 3 && @fun > 0
     return @fun += 1 if (@hygiene != 4 || @food != 4) && @fun < 4
-    return @fun += 1 if (@hygiene == 4 || @food == 4) && (@fun == 3 || @fun.zero?)
+    return @fun += 1 if level_max_two_item?(@hygiene, @food) && (@fun == 3 || @fun.zero?)
   end
 
   def time_food
-    return @food *= 2 if (@hygiene == 4 || @fun == 4) && @food < 3 && @food > 0
+    return @food *= 2 if level_max_two_item?(@hygiene, @fun) && @food < 3 && @food > 0
     return @food += 1 if (@hygiene != 4 || @fun != 4) && @food < 4
-    return @food += 1 if (@hygiene == 4 || @fun == 4) && (@food == 3 || @food.zero?)
+    return @food += 1 if level_max_two_item?(@hygiene, @fun) && (@food == 3 || @food.zero?)
+  end
+
+  def level_max_two_item?(item1, item2)
+    return true if item1 == 4 || item2 == 4
+    false
   end
 
   def level_max
